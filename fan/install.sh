822d3cf9c3ca7c85a395357d9945fc40351253a7
ZYZYSK <huangtianhe1423x@gmail.com>
2022-08-15 18:09:50 +0900

    fixed bugs

--- fan/install.sh
+++ fan/install.sh
@@ -11,7 +11,7 @@ sudo cp ./fan_controller.py /opt/fan/fan_controller.py
 sudo cp ./settings.json /opt/fan/settings.json
 sudo cp ./fan_controller.service /etc/systemd/system/fan_controller.service
 
-sudo chmod 0775 /opt/fan
+# sudo chmod 0777 /opt/fan
 sudo chmod 0755 /opt/fan/fan_controller.py
-sudo chmod 0664 /opt/fan/settings.json
+# sudo chmod 0666 /opt/fan/settings.json
 sudo systemctl enable fan_controller.service --now
